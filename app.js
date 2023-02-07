25b281ec64058e5cd8ec95fdee49bb95ff980dbc
elicadv <elicavb@gmail.com>
2023-02-07 11:23:53 -0300

    finalizado

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import express from 'express'
+import cors from "cors"
 import pousadaController from './controller/pousada_controller.js'
 
 
 const app = express()
+app.use(cors())
 const port = 3009
 
 app.use(express.json())
 
 pousadaController(app)
 
-app.listen (port, () => {
+app.listen (process.env.PORT || 3009, () => {
     console.log(`http://localhost:${port}/`)
 })
 
